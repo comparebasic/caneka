e4d7b9af5c4d5d77a246ff2bf03281e4f961a191
Firecrow Silvernight <stephen@firecrow.com>
2024-06-21 21:15:13 -0500

    bringing filestore code over from framework-apaloozo and from cycleserve

---
+++ array.h
@@ -0,0 +1,3 @@
+void **Array_Make(MemCtx *m, int count);
+void *Array_MakeFrom(MemCtx *m, int count, ...);
+int Array_Length(void **arr);
---
+++ error.h
@@ -0,0 +1,2 @@
+void Fatal(char *msg, type t);
+void Error(char *msg);
