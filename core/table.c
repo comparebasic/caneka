9c7a52b74f3e20cb85ba433c5a805255022ea935
Firecrow Silvernight <stephen@firecrow.com>
2024-06-27 22:02:27 -0500

    creating table algorith

--- core/table.c
+++ core/table.c
@@ -1,20 +1,18 @@
 #include <external.h>
 #include <caneka.h>
 
-/* 
-This is a fomula to take parts of the has for the coordinates of different levels of the
-has table, each represented by a span with different dimensions
-*/
-static int getTieredHash(util hash, byte level){
-    return (byte) ((hash >> (level*4)) & TABLE_MOD);
-}
-
 Abstract *Table_Get(MemCtx *m, Table *tbl, Abstract *a){
     Hashed *h = Hashed_Make(m, a);
-    if(h != NULL){
-        byte b = getTieredHash(h->id, 0);
-        Bits_Print(&b, sizeof(byte), "Hash level 0", COLOR_CYAN);
-        printf("\n");
+
+    Abstract *v = NULL;
+    boolean found = FALSE;
+    for(int i = 0; i < tbl->values->dims && i < TABLE_MAX_DIMS; i++){
+        int hkey = h->id & TABLE_DIM_LOOKUPS[i];
+        Abstract *v = Span_Get(tbl->values, hkey);
+        if(Compare(a, b)){
+            found = TRUE;
+            break;
+        }
     }
-    return NULL;
+    return v;
 }
--- include/table.h
+++ include/table.h
@@ -1,15 +1,9 @@
-#define TABLE_MOD 15
-enum table_levels {
-    LEVEL_ONE = 0,
-    LEVEL_TWO = 1,
-    LEVEL_THREE = 2,
-    LEVEL_FOUR = 3,
-};
+#define TABLE_MAX_DIMS 5
+int TABLE_DIM_LOOKUPS[5] = {15, 16*16, 16*16*16, 16*16*16*16, 16*16*16*16*16};
 
 typedef struct table {
     Type type;
     Span *values;
-    byte level;
 } Table;
 
 Abstract *Table_Get(MemCtx *m, Table *tbl, Abstract *a);
