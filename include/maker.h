ca543c9c077787d189165b7a4a29d2b9ebadd6bb
Firecrow Silvernight <stephen@firecrow.com>
2024-07-14 01:13:46 -0500

    removing maker in favor of literal ptr

--- include/maker.h
+++
@@ -1,7 +0,0 @@
-typedef Abstract *(*Maker)(struct mem_ctx *m, Abstract *v);
-Abstract *Maker_Make(struct mem_ctx *m, void *mk, cls type);
-
-typedef struct maker {
-    Type type;;
-    Maker *mk;
-} MakerObj;
