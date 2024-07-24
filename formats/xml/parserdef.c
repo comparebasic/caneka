642dac0eea1acd4a706905f68d883f5004a79dbe
Firecrow Silvernight <stephen@firecrow.com>
2024-07-24 10:23:16 -0500

    starting to sketch about xml parsing

---
+++ formats/xml/parserdef.c
@@ -0,0 +1,15 @@
+#include <external.h>
+#include <caneka.h>
+
+/*
+Span *XmlParser_Make(MemCtx *m, ProtoDef *def){
+    Span *p =  Span_From(m, 6, 
+        (Abstract *)headerMarkMk,
+        (Abstract *)keyParserMk,
+        (Abstract *)hdrSepParserMk,
+        (Abstract *)hdrBodyParserMk,
+        (Abstract *)hdrNlParserMk,
+        (Abstract *)nlDblParserMk);
+    return p;
+}
+*/
