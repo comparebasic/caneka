5324ce6041e5bd07a39ddcc91fe24d5ffd64ab8a
Firecrow Silvernight <fire@firecrow.com>
2025-02-21 11:55:34 -0600

    moving signer and rblsh into seperate repos

--- dist/src/programs/signer/signer_init.c
+++
@@ -1,9 +0,0 @@
-#include <external.h>
-#include <caneka.h>
-#include <signer.h>
-
-status Signer_Init(MemCtx *m){
-    status r = READY;
-    r |= Lookup_Add(m, DebugPrintChain->funcs, TYPE_SIGNER_CTX, (void *)SignerCtx_Print);
-    return r;
-}
