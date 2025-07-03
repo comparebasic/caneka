aa0c5aad53b8404c4acdb7c8bf139fa99c6143e1
Firecrow Silvernight <fire@firecrow.com>
2025-07-03 00:57:05 +0000

    sketching cash

--- artifact/src/ext/format/cash/cash.c
+++ artifact/src/ext/format/cash/cash.c
@@ -1,14 +1,23 @@
 #include <external.h>
 #include <caneka.h>
 
-Cash *Cash_Make(MemCtx *m, Getter get, Abstract *source){
+status CashCtx_StreamTo(Stream *sm, byte *b, i32 length){
+    Stream_Bytes(sm, b, length); 
+    CashCtx *ctx = as(sm->source, TYPE_CASH_CTX);
+    return Roebling_Run(ctx->rbl);
+}
+
+Cash *Cash_Make(MemCtx *m, CashCtx *ctx, Abstract *source){
     Cash *o = (Cash*)MemCtx_Alloc(m, sizeof(Cash));
     o->type.of = TYPE_CASH;
-    o->m = m;
-    /* Roebling = /between({^$.},$ -> value),value({_\{^\}.} -> between)/ */
-    o->rbl = Cash_RblMake_rbl(m, (Abstract *)o, Cash_Capture);
-    o->get = get;
-    o->source = source;
-
+    ctx->cash = o;
     return o;
+}
+
+CashCtx *CashCtx_Make(MemCtx *m, Stream *sm,  Abstract *source){
+    CashCtx *ctx = (CashCtx*)MemCh_Alloc(m, sizeof(CashCtx));
+    ctx->type.of = TYPE_CASH_CTX;
+    ctx->rbl = Cash_RoeblingMake(m, sm->dest.curs, source);
+
+    return ctx;
 }
---
+++ artifact/src/ext/format/cash/cash_roebling.c
@@ -0,0 +1,83 @@
+#include <external.h>
+#include <caneka.h>
+
+static PatCharDef textDef[] = {
+    {PAT_KO|PAT_KO_TERM,'$' ,'$'},
+    patText,
+    {PAT_END, 0, 0}
+};
+
+static PatCharDef varDef[] = {
+    {PAT_TERM, '$' ,'$'},{PAT_TERM|PAT_INVERT|PAT_INVERT_CAPTURE, '{', '{'},
+    {PAT_END, 0, 0}
+};
+
+static PatCharDef varKeyDef[] = {
+    {PAT_TERM, '$' ,'$'},
+    {PAT_TERM|PAT_INVERT_CAPTURE, '{', '{'},
+    {PAT_KO|PAT_KO_TERM, '}', '}'},
+    patText,
+    {PAT_END, 0, 0}
+};
+
+static PatCharDef varIDXDef[] = {
+    {PAT_TERM, '$' ,'$'},{PAT_TERM|PAT_INVERT_CAPTURE, '[', '['},
+    {PAT_KO|PAT_KO_TERM, ']', ']'},
+    {PAT_MANY|PAT_TERM, '0', '9'},
+    {PAT_END, 0, 0}
+};
+
+static status text(MemCh *m, Roebling *rbl){
+    status r = READY;
+    Roebling_ResetPatterns(rbl);
+
+    r |= Roebling_SetPattern(rbl,
+        textDef, FORMAT_CASH_TEXT, FORMATTER_CASH_VAR);
+
+    return r;
+}
+
+static status var(MemCh *m, Roebling *rbl){
+    status r = READY;
+    Roebling_ResetPatterns(rbl);
+
+    r |= Roebling_SetPattern(rbl,
+        varDef, FORMAT_CASH_VAR, FORMAT_CASH_TEXT);
+    r |= Roebling_SetPattern(rbl,
+        varKeyDef, FORMAT_CASH_VAR_KEY, FORMAT_CASH_TEXT);
+    r |= Roebling_SetPattern(rbl,
+        varIdxDef, FORMAT_CASH_VAR_IDX, FORMAT_CASH_TEXT);
+
+    return r;
+}
+
+static status Capture(Roebling *rbl, word captureKey, StrVec *v){
+    Mess *mess = rbl->mess;
+    Tokenize *tk = Lookup_Get(mess->tokenizer, captureKey);
+    if(rbl->mess->type.state & DEBUG){
+        Abstract *args[] = {
+            (Abstract *)Type_ToStr(OutStream->m, captureKey),
+            (Abstract *)v,
+            (Abstract *)tk,
+            NULL
+        };
+        Out("^c.Fmt After Capture ^E0.$^ec./@ -> @\n", args);
+    }
+    return SUCCESS;
+}
+
+Roebling *Cash_RoeblingMake(MemCh *m, Cursor *curs, Abstract *source){
+    Roebling *rbl = Roebling_Make(m, curs, Capture, NULL); 
+    Roebling_AddStep(rbl, (Abstract *)I16_Wrapped(m, FORMAT_CASH_TEXT));
+    Roebling_AddStep(rbl, (Abstract *)Do_Wrapped(m, (DoFunc)text));
+    Roebling_AddStep(rbl, (Abstract *)I16_Wrapped(m, FORMAT_CASH_VAR));
+    Roebling_AddStep(rbl, (Abstract *)Do_Wrapped(m, (DoFunc)var));
+    Roebling_Start(rbl);
+
+    rbl->capture = Capture;
+
+    rbl->mess = Mess_Make(m);
+    rbl->mess->tokenizer = NULL;
+    rbl->source = source;
+    return rbl;
+}
