5639114b6e3f7ca4545fd75895fd8bd300f78cb4
Firecrow Silvernight <fire@firecrow.com>
2025-09-30 16:02:38 +0000

    sketching out persist blanking addresses

--- artifact/src/base/io/persist.c
+++ artifact/src/base/io/persist.c
@@ -15,10 +15,9 @@ cls Persist_UnpackAddr(MemCh *pm, void **ptr){
     return typeOf;
 }
 
-status Persist_PackAddr(MemCh *m, Table *tbl, i32 slIdx, void **ptr, cls typeOf){
+status Persist_PackAddr(cls typeOf, i32 slIdx, void **ptr){
     util u = (util)ptr;
     u &= ~MEM_PERSIST_MASK;
-    PersistItem *pageItem = (PersistItem *)Table_Get(tbl, (Abstract *)Util_Wrapped(m, u));
 
     PersistCoord coord;
     PersistCoord_Fill(&coord, pageItem->coord.idx, *ptr, typeOf);
@@ -39,46 +38,47 @@ status Persist_FlushFree(Stream *sm, MemCh *persist){
     MemIter_Init(&mit, persist);
     while((MemIter_Next(&mit) & END) == 0){
         Abstract *a = MemIter_Get(&mit);
-        Table_Set(tbl, (Abstract *)Util_Wrapped(m, (util)a), 
-            (Abstract *)PersistItem_Make(m, mit.slIdx, (void *)a, a->type.of));
+        if((mit.type.state & MORE) == 0){
+            Table_Set(tbl, (Abstract *)Util_Wrapped(m, (util)a), 
+                (Abstract *)PersistItem_Make(m, mit.slIdx, (void *)a, a->type.of));
+        }
     }
 
     args[0] = (Abstract *)tbl;
     args[1] = NULL;
     Out("^y.MemTable &\n", args);
 
-    /*
-    Iter_Reset(&persist->it);
-    while((Iter_Next(&persist->it) & END) == 0){
-        MemPage *pg = Iter_Get(&persist->it);
-        void *end = pg+PAGE_SIZE-1;
-        void *ptr = ptr+((util)pg->remaining); 
-        while(ptr <= end){
-            Abstract *a = (Abstract *)ptr;
+    MemIter_Init(&mit, m);
+    while((MemIter_Next(&mit) & END) == 0){
+        Abstract *a = MemIter_Get(&mit);
+        if((mit.type.state & MORE) == 0){
             Map *map = (Map *)Lookup_Get(MapsLookup, a->type.of);
             if(map == NULL){
                 Error(ErrStream->m, (Abstract *)persist, FUNCNAME, FILENAME, LINENUMBER,
                     "Map not found, needed for mem persist", NULL);
                 return ERROR;
             }
+            PersistItem *item = (PersistItem *)Table_Get(tbl, Util_Wrapped(m, (util)a));
+            Persist_PackAddr(item->typeOf, slIdx, (void **)&a);
+
             for(i16 i = 1; i < map->type.range; i++){
                 RangeType *att = map->atts+i;
                 if(att->of > _TYPE_RAW_END){
-                    Persist_PackAddr(m,
-                        tbl,
-                        persist->it.idx,
-                        ((void *)a)+att->range,
-                        att->of);
+                    Abstract *aa = ((void *)a)+att->range;
+                    item = (PersistItem *)Table_Get(tbl, Util_Wrapped(m, (util)a));
+                    Persist_PackAddr(item->typeOf, slIdx, (void **)&aa);
                 }
             }
             a->type.of = TYPE_BLANKED;
-            Single *size = (Single *)Lookup_Get(SizeLookup, a->type.of);
-            ptr += size->val.value;
         }
     }
 
+    Iter_Reset(&persist->it);
+    while((Iter_Next(&persist->it) & END) == 0){
+        /* write blanked pages to stream */
+    }
+
     MemCh_Free(m);
-    */
     return r;
 }
 
