b288f10d73479f74f8119bc9c28b4570878c0651
Firecrow Silvernight <fire@firecrow.com>
2025-10-04 21:09:05 +0000

    sketching queue set criteria

--- artifact/src/ext/include/navigate/queue.h
+++ artifact/src/ext/include/navigate/queue.h
@@ -21,7 +21,5 @@ Queue *Queue_Make(MemCh *m);
 status Queue_AddHandler(Queue *q, QueueCrit *crit);
 #define *Queue_GetMem((q)) ((q)->itemsIt.p->m)
 #define Queue_Get((q)) ((q)->value)
-status Queue_SetCriteria(Queue *q, i32 critIdx, i32 idx, util value);
-status Queue_SetHalfCriteria(Queue *q, i32 critIdx, i32 idx, quad value);
-gobits Queue_CheckSlab(Queue *q);
+status Queue_SetCriteria(Queue *q, i32 critIdx, i32 idx, void *value);
 status Queue_Next(Queue *q);
--- artifact/src/ext/navigate/queue.c
+++ artifact/src/ext/navigate/queue.c
@@ -1,6 +1,36 @@
 #include <external.h>
 #include <caneka.h>
 
+static gobits Queue_CheckSlab(Queue *q);
+
+status Queue_SetCriteria(Queue *q, i32 critIdx, i32 idx, void *value){
+    status r = READY;
+    i32 slabIdx = idx / SPAN_STRIDE;
+    MemCh *m = Queue_GetMem(q);
+    void **slab = NULL;
+    boolean half = (q->type.state & QUEUE_CRIT_HALF) != 0; 
+    if((slab = Span_Get(q->crit.data, slabIdx)) == NULL){
+        size_t sz = (half ? sizeof(word) : sizeof(util));
+        slab = MemCh_Alloc(m, sizeof(sz*SPAN_STRIDE); 
+        r |= Span_Set(q->crit.data, slabIdx, (Abstract *)slab);
+    } 
+    i32 localIdx = idx-slabIdx*SPAN_STRIDE;
+    if(half){
+        void *hslab = (word *)
+        hslab[localIdx] = *(word *)value;
+        r |= SUCCESS;
+    }else{
+        void *uslab = (util *)
+        uslab[localIdx] = *(util *)value;
+        r |= SUCCESS;
+    }
+    return r;
+}
+
+status Queue_Next(Queue *q){
+    ;
+}
+
 status Queue_AddHandler(Queue *q, QueueCrit *crit){
     status r = READY;
     r |= Span_Add(q->crit.handlers, (Abstract *)crit);
