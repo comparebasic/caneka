2e36bc659bc54186df16298c0ed50716ba5379d6
Firecrow Silvernight <fire@firecrow.com>
2025-11-16 00:16:10 +0000

    un-rebuilding during test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,13 +3,6 @@ CC="clang"
 INC="-I ./src/include -I src/base/include -I src/builder/include"
 STATICS="build/libcaneka/libcaneka.a build/libbuilder/libbuilder.a"
 
-./scripts/make.sh
-r=$?
-
-if [ $r -ne 0 ]; then
-    exit $r;
-fi
-
 echo "building Tests Config..." && \
 $CC -o build/programs_tests_build $INC $STATICS src/programs/tests/build.c -lm
 
