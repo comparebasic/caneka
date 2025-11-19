929c0945548ff813cf5dc53bc30cc923801c31f5
Firecrow Silvernight <fire@firecrow.com>
2025-11-19 05:27:24 +0000

    updating content

--- scripts/demo-content.sh
+++ scripts/demo-content.sh
@@ -2,6 +2,9 @@
 ./build/bin/tests --dist
 
 cp -v dist/output/tests.fmt examples/web-server/pages/public/tests.fmt
+cp -v dist/output/tests.fmt examples/web-server/pages/static/tests-fmt.txt
 cp -v docs/schedule.fmt examples/web-server/pages/public/schedule.fmt
 cp -v docs/about.fmt examples/web-server/pages/public/index.fmt
 cp -v docs/about.fmt examples/web-server/pages/static/about-fmt.txt
+cp -v examples/web-server/pages/public/stats.templ examples/web-server/pages/static/stats-templ.txt
+cp -v examples/web-server/pages/nav.config examples/web-server/pages/static/nav-config.txt
