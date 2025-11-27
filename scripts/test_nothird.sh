8c2a3f723472e3cd8f6e5850706b4047d7fbcfa1
Firecrow Silvernight <fire@firecrow.com>
2025-11-27 16:06:15 -0600

    starting on a nothird dependencies build script

--- scripts/test_nothird.sh
+++ scripts/test_nothird.sh
@@ -4,6 +4,7 @@ INC="-I ./src/include -I src/base/include -I src/builder/include"
 STATICS="build/libcaneka/libcaneka.a build/libbuilder/libbuilder.a"
 
 echo "building Tests Config..." && \
+rm -f build/programs_tests_build
 $CC -o build/programs_tests_build $INC $STATICS -DBASE -DEXT -DINTER \
     src/programs/tests/build.c -lm
 
