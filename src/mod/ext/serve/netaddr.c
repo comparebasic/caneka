87b083ee682dab23c0e1073a5b51d1dbf1d9d480
Firecrow Silvernight <fire@firecrow.com>
2026-07-29 19:38:30 -0500

    re-organizing web config file

--- src/mod/ext/serve/netaddr.c
+++ src/mod/ext/serve/netaddr.c
@@ -7,6 +7,21 @@ NetAddr *NetAddr_Make4(MemCh *m){
     return addr;
 }
 
+void NetAddr_SetFromStr4(NetAddr *net, Str *s){
+    if(net->type.of != TYPE_NET_ADDR4){
+        void *ar[] = {net, NULL};
+        Error(m, FUNCNAME, FILENAME, LINENUMBER,
+            "Trying to set ip4 of another address type", ar);
+        return;
+    }
+
+    net->ip4addr.sin_addr.s_addr = Str_ToIp4(s);
+}
+
+void NetAddr_SetFromStrPort(NetAddr *net, Str *s){
+    net->port = ;
+}
+
 NetAddr *NetAddr_Make6(MemCh *m){
     NetAddr *addr = MemCh_AllocOf(m, sizeof(NetAddr), TYPE_NET_ADDR6);
     addr->type.of = TYPE_NET_ADDR6;
